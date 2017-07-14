fa69cd51dc393dfde4871b63e7b7f2186c09d37b
Shervin Khastoo <shervin@shervin.org>
2017-07-14 19:02:12 +0430

    Fix hot reload

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -26,6 +26,11 @@ config.devServer = {
   compress: true,
   historyApiFallback: true,
   disableHostCheck: true,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+    'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+  }
 }
 
 module.exports = config
