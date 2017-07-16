73de7041c9c6de501eff04a0d96fc5d835b9d551
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-16 19:14:27 +0430

    Change Rahnama to Rehname

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -3,6 +3,6 @@ import { Header, Segment } from 'semantic-ui-react'
 
 export default function AppHeader() {
   return <Segment className='app__header'>
-    <Header>سامانه‌ی راهنما</Header>
+    <Header>سامانه‌ی رهنما</Header>
   </Segment>
 }
