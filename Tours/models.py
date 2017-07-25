271302ce339f585eee0ded99a68cd7d26cc5c838
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-25 12:33:55 +0430

    changes to TOUR data base

--- Tours/models.py
+++ Tours/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
-import user
+import django
+from user.models import User
 from django.db import models
 
 
@@ -8,17 +9,18 @@ from django.db import models
 
 class Tour(models.Model):
     name = models.CharField(max_length=100)
-    start_date = models.DateField(default=datetime.now())
-    end_date = models.DateField(default=datetime.now())
-    price = models.IntegerField(max_length=10)
+    start_date = models.DateField(default=django.utils.timezone.now)
+    end_date = models.DateField(default=django.utils.timezone.now)
+    price = models.IntegerField()
     spec = models.TextField(max_length=2000)
+    capacity = models.IntegerField()
 
     def __str__(self):
         return str(self.name)
 
 
 class ReviewTour(models.Model):
-    student_id = models.ForeignKey(user, on_delete=models.CASCADE, default=1)
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     tour_id = models.ForeignKey(Tour, on_delete=models.CASCADE, default=1)
     date = models.DateField(default=datetime.now)
     text = models.TextField(max_length=2000)
@@ -28,7 +30,7 @@ class ReviewTour(models.Model):
 
 
 class ReserveTour(models.Model):
-    student_id = models.ForeignKey(user, on_delete=models.CASCADE, default=1)
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     tour_id = models.ForeignKey(Tour, on_delete=models.CASCADE, default=1)
     status = models.CharField(max_length=1000)
 
@@ -37,10 +39,18 @@ class ReserveTour(models.Model):
 
 
 class RequestForTour(models.Model):
-    student_id = models.ForeignKey(user, on_delete=models.CASCADE, default=1)
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     tour_id = models.ForeignKey(Tour, on_delete=models.CASCADE, default=1)
     date = models.DateField(default=datetime.now)
     status = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Comments(models.Model):
+    tour_id = models.ForeignKey(Tour, on_delete=models.CASCADE)
+    comment_text = models.TextField(max_length=1000)
 
     def __str__(self):
         return str(self.id)
--- Tours/views.py
+++ Tours/views.py
@@ -1,19 +1,83 @@
-from Tours.models import Tour
+import Tours
+from Tours.models import Tour, ReserveTour
+from django.http import JsonResponse, Http404
 from django.shortcuts import render
 
 
-# Create your views here.
-
 def searchTour(request):
-    name = "shomal"
-    tour = Tour.objects.filter(name__contains=name)
+    if request.method == 'POST':
+        print("hi")
+    bodyParams = request.body.decode('utf-8').split('&')
+    print("hi")
+    print(bodyParams)
+    """tour = Tour.objects.filter(name__contains=name)
+    if len(tour) == 0:
+        return JsonResponse({'status': "-1", 'message': "No Tour Found"})
+    if len(tour) >= 1:
+        response = {
+            "status": 0,
+            "tours": [{'id': str(tour[0].id), 'name': tour[0].name, 'start_time': str(tour[0].start_date),
+                       'end_time': str(tour[0].end_date), 'price': str(tour[0].price)}]
+        }
+        if len(tour) == 1:
+            return JsonResponse(response)
+        i = 0
+        while i < tour.count():
+            response['tours'] = response['tours'] + [{'id': str(tour[0].id), 'name': tour[0].name, 'start_time': str(tour[0].start_date),
+                       'end_time': str(tour[0].end_date), 'price': str(tour[0].price)}]
+            i = i + 1
+        return JsonResponse(response)"""
+
+    return JsonResponse({'status': "0", 'message': "No Tour Found"})
+
+
+def getTour(request, id):
+    tour = Tour.objects.filter(id=id)
+    if tour.count() == 0:
+        return JsonResponse({'status': "-1", 'message': "No such post Found"})
+
     response = {
         "status": 0,
-        "posts": [{'id': str(tour[0].id), 'name': tour[0].name, 'start_time': str(tour[0].start_date),
-                   'end_time': str(tour[0].end_date), 'price': str(tour[0].price), 'spec':tour[0].spec}]
+        "tours": [{'id': str(tour[0].id), 'name': tour[0].name, 'start_time': str(tour[0].start_date),
+                       'end_time': str(tour[0].end_date), 'price': str(tour[0].price)}]
     }
-    while i < posts.count():
-        response['posts'] = response['posts'] + [
-            {'id': str(posts[i].id), 'title': posts[i].title, 'summery': posts[i].summary,
-             'datetime': str(posts[i].date)}]
-        i = i + 1
+    print(response)
+    return JsonResponse(response)
+
+
+def reserveTour(request, id):
+    tour = Tour.objects.filter(id=id)
+    #to be complete
+    std_id = 1
+    if len(tour) == 0:
+        return JsonResponse({'status': "-1", 'message': "No Tour Found"})
+
+    if not tour[0].capacity == 0:
+        tour[0].capacity = tour[0].capacity - 1
+        tour[0].save()
+        reserve = ReserveTour.objects.create()
+        reserve.student_id = std_id
+        reserve.tour_id = id
+        reserve.status = 0
+        reserve.save()
+        return JsonResponse({'status': "0", 'message': "tour reserved"})
+    else:
+        return JsonResponse({'status': "0", 'message': "No Capacity for this Tour Found"})
+
+
+def cancelReserve(request, id):
+    # to be complete
+    std_id = 1
+    tour = Tour.objects.filter(id=id)
+    reserve = ReserveTour.objects.filter(student_id=std_id, tour_id=id)
+    if len(tour) == 0:
+        return JsonResponse({'status': "-1", 'message': "No Tour Found"})
+
+    if len(reserve) == 0:
+        return JsonResponse({'status': "-1", 'message': "This reserve does not exist!"})
+    tour[0].capacity = tour[0].capacity + 1
+    tour[0].save()
+    reserve[0].delete()
+    return JsonResponse({'status': "0", 'message': "reserve canceled!"})
+
+
