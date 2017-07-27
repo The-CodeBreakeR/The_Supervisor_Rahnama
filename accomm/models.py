6180fa79d5a66696546edb6cba4ee19be4ea1aac
Seyed Ali Tabatabaee <Ali>
2017-07-27 08:31:44 -0700

    First version of accoms' models is finished

--- accomm/models.py
+++ accomm/models.py
@@ -7,9 +7,22 @@ from django.db import models
 # Create your models here.
 
 class AccommPrinciple(models.Model):
-    rule_id = models.IntegerField()
+    rule_id = models.IntegerField(primary_key=True)
     date = models.DateField(default=datetime.now)
     description = models.TextField(max_length=2000)
 
     def __str__(self):
         return str(self.description)
+
+class Accommodation(models.Model):
+    acc_id = models.IntegerField(primary_key=True)
+    size = models.IntegerField()
+    type = models.TextField(max_length=30)
+    location = models.TextField(max_length=2000)
+    contracted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    start_date = models.DateField(default=datetime.now, null=True)
+    end_date = models.DateField(default=datetime.now, null=True)
+    reserved_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return str(self.id)
