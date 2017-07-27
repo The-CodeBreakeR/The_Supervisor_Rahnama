d62d3cdb0c41c3b43a06fb7a75e2e956b7b4d599
Seyed Ali Tabatabaee <Ali>
2017-07-27 09:43:39 -0700

    further updates on models.py

--- account/models.py
+++ account/models.py
@@ -1,3 +1,34 @@
+from datetime import datetime
+
+import django
+from user.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Income(models.Model):
+    income_id = models.IntegerField()
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    amount = models.IntegerField()
+    source = models.CharField(max_length=100)
+
+class Expense(models.Model):
+    expense_id = models.IntegerField()
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    amount = models.IntegerField()
+    destination = models.CharField(max_length=100)
+
+class LoanRequest(models.Model):
+    request_id = models.IntegerField()
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    amount = models.IntegerField()
+    purpose = models.CharField(max_length=100)
+
+class LoanResponse(models.Model):
+    response_id = models.IntegerField()
+    request_id = models.ForeignKey(LoanRequest, on_delete=models.CASCADE, default=1)
+    student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+    answer = models.CharField(max_length=100)
+    repayment_period = models.IntegerField(null=True)
+    repayment_rate = models.IntegerField(null=True)
