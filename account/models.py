a93206fc1a077a628b04a928b0842d0f59cbfdb6
Seyed Ali Tabatabaee <Ali>
2017-07-28 04:52:14 -0700

    Primary keys added

--- account/models.py
+++ account/models.py
@@ -8,25 +8,28 @@ from django.db import models
 # Create your models here.
 
 class Income(models.Model):
-    income_id = models.IntegerField()
+    income_id = models.IntegerField(primary_key=True)
     student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     amount = models.IntegerField()
     source = models.CharField(max_length=100)
 
+    def __str__(self):
+        return str(self.id)
+
 class Expense(models.Model):
-    expense_id = models.IntegerField()
+    expense_id = models.IntegerField(primary_key=True)
     student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     amount = models.IntegerField()
     destination = models.CharField(max_length=100)
 
 class LoanRequest(models.Model):
-    request_id = models.IntegerField()
+    request_id = models.IntegerField(primary_key=True)
     student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     amount = models.IntegerField()
     purpose = models.CharField(max_length=100)
 
 class LoanResponse(models.Model):
-    response_id = models.IntegerField()
+    response_id = models.IntegerField(primary_key=True)
     request_id = models.ForeignKey(LoanRequest, on_delete=models.CASCADE, default=1)
     student_id = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     answer = models.CharField(max_length=100)
