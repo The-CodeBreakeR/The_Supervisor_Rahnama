c6e53283a063fc2a4b757321fec6c16a569141ca
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-29 22:43:39 +0430

    changes to client/srts/compnenets/App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import {Route, Link} from 'react-router-dom'
+import MomentJ from 'moment-jalaali'
 
 import AppHeader from './AppHeader'
+import ToursHome from './tours/ToursHome'
+
+MomentJ.loadPersian()
 
 class App extends React.Component {
   render() {
     return <div className='app'>
       <AppHeader/>
-      <Route path="/test" component={AppHeader}/>
+      <Route path="/tours" component={ToursHome}/>
     </div>
   }
 }
