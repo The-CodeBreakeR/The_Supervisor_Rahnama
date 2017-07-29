ed66b6d3826f1a92e201051fe274edd3e64ddf0c
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-29 17:18:53 +0430

    add Tours/admin

--- Tours/admin.py
+++ Tours/admin.py
@@ -1,3 +1,10 @@
+from .models import Tour
 from django.contrib import admin
 
+
 # Register your models here.
+class TourAdmin(admin.ModelAdmin):
+    list_display = ['name', 'start_date', 'end_date', 'price', 'capacity']
+
+
+admin.site.register(Tour, TourAdmin)
