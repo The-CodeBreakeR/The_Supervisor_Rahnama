fa375c32d9110ccfeefcbec082a37ee7b726c4b8
Seyed Ali Tabatabaee <Ali>
2017-07-30 13:10:40 -0700

    update admin.py

--- accomm/admin.py
+++ accomm/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from accomm.models import AccommPrinciple, Accommodation
+
+admin.site.register(Accommodation)
+admin.site.register(AccommPrinciple)
