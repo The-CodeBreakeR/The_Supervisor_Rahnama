f17330a0b3aa52daccd804cfdc6e8f0526ffd2ae
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-30 20:01:29 +0430

    change canselReservationButton

--- client/src/components/tours/CanselReserveButton.js
+++ client/src/components/tours/CanselReserveButton.js
@@ -1,20 +1,76 @@
 import React from 'react'
-import { Button, Header, Icon, Image, Modal } from 'semantic-ui-react'
+import { Button, Header, Icon, Image, Modal, Input } from 'semantic-ui-react'
 import _ from 'lodash'
 import Strings from '../../localization'
+import Cookie from 'browser-cookies'
 
 class CanselReserveButton extends React.Component {
-  render() {
-    return <Modal trigger={<Button negative >{Strings.tourCansel}</Button>}>
+  constructor(props) {
+    super(props)
+    this.state = {
+      open: false,
+      status: 0,
+      message: '',
+    }
+  }
+
+  handleResault (status) {
+    this.setState({status: 1})
+    this.setState({message: Strings.requestSubmitAccept})
+  }
+
+  resetState () {
+    this.state = {
+      message: '',
+      status: 0,
+    }
+  }
+
+  close() {
+    this.setState({open: false})
+    this.resetState()
+    this.props.getInfoRecall()
+  }
+
+  handleClose () {
+    this.setState({close: false})
+  }
+
+  ButtonClickHandle (tourId) {
+    if (Cookie.get('token')) {
+      fetch('/tours/cansel/', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          token: JSON.parse(localStorage.getItem('user')).token,
+          tourId: tourId,
+        }),
+      })
+        .then(response => response.json())
+        .then(result => this.handleResault())
+    } else {
+      this.setState({message: Strings.haveNotRegister})
+    }
+  }
+
+  render () {
+    return <Modal trigger={<Button
+      positive /*onClick={() => this.ButtonClickHandle(this.props.tourId)}*/ >{Strings.tourCansel}</Button>}
+    open={this.state.open}
+    onOpen={() => this.setState({open: true})}
+    >
       <Modal.Header>{Strings.tourCansel}</Modal.Header>
       <Modal.Content image scrolling>
         <Modal.Description>
-          <Header>{Strings.tourCansel}</Header>
+          <p>{this.state.message}</p>
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
-        <Button >
-          {Strings.tourAccepted}
+        <Button onClick={() => this.close()}>
+          {Strings.tourStop}
         </Button>
       </Modal.Actions>
     </Modal>
