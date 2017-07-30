8a29d6bb98fb2141885835d59b674805656c6167
Seyed Ali Tabatabaee <Ali>
2017-07-30 13:13:38 -0700

    update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from account.models import Expense, LoanResponse, Income, LoanRequest
+
+admin.site.register(Income)
+admin.site.register(Expense)
+admin.site.register(LoanRequest)
+admin.site.register(LoanResponse)
