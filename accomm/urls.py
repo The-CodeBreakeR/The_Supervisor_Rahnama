07149997e7e3e79365d84b30a7abfd8e273aee31
Seyed Ali Tabatabaee <Ali>
2017-07-30 12:37:03 -0700

    edit urls.py

--- accomm/urls.py
+++ accomm/urls.py
@@ -1,8 +1,8 @@
 import accomm.views
 from django.conf.urls import url
 
-urlpatterns = {
+urlpatterns = [
     url(r'^rules/', accomm.views.showRules, name='showRules'),
     url(r'^places/', accomm.views.availablePlaces, name='availablePlaces'),
     url(r'^reserve/(\d+)', accomm.views.reserveAPlace, name='reserveAPlace'),
-}
+]
