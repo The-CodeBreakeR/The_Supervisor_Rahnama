2b37e0a8c75904f354d67b064415e3f430c8da54
Seyed Ali Tabatabaee <Ali>
2017-07-30 12:50:56 -0700

    change to urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 import account.views
 from django.conf.urls import url
 
-urlpatterns = {
+urlpatterns = [
     url(r'^getincome/', account.views.getIncome, name='getIncome'),
     url(r'^newincome/', account.views.newIncome, name='newIncome'),
     url(r'^getexpense/', account.views.getExpense, name='getExpense'),
     url(r'^newexpense/', account.views.newExpense, name='newExpense'),
     url(r'^loanrequest/', account.views.newRequest, name='newRequest'),
     url(r'^response/(\d+)', account.views.getResponse, name='getResponse'),
-}
+]
