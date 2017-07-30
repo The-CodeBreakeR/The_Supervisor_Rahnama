79bbec19997848159cede2b1b4a014e9429b3eea
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-30 18:19:04 +0430

    change Tours/url

--- Tours/url.py
+++ Tours/url.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^search/', Tours.views.searchTour, name='searchTour'),
-    url(r'^getTour/', Tours.views.getTour, name='getTour')
+    url(r'^getTour/', Tours.views.getTour, name='getTour'),
+    url(r'^reserve/', Tours.views.reserveTour, name='reserve'),
 
 ]
