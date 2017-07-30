43f700e43bd71dc8695bca074d3b62698d51e307
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-07-30 12:24:16 +0430

    update ToursList

--- client/src/components/tours/ToursList.js
+++ client/src/components/tours/ToursList.js
@@ -2,22 +2,29 @@ import React from 'react'
 import { Table } from 'semantic-ui-react'
 import Strings from '../../localization'
 import MomentJ from 'moment-jalaali'
+import ToursInfo from './ToursInfo'
 
 class ToursList extends React.Component {
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     TourName: '',
+  //   }
+  // }
   renderTour(tour) {
-    return <Table.Row key={tour.id}>
+    return <Table.Row key={tour.id} >
       <Table.Cell>{tour.id} </Table.Cell>
       <Table.Cell>{tour.name}</Table.Cell>
       <Table.Cell>{MomentJ(tour.start_time * 1000).format('LLLL')}</Table.Cell>
       <Table.Cell>{MomentJ(tour.end_time * 1000).format('LLLL')}</Table.Cell>
       <Table.Cell>{tour.price}</Table.Cell>
+      <ToursInfo tourId={tour.id} />
     </Table.Row>
   }
-
   render() {
     const tours = this.props.toursList.map((tour) => this.renderTour(tour))
     return <div>
-      <Table basic='very'>
+      <Table basic='very' celled selectable>
         <Table.Header>
           <Table.Row>
             <Table.HeaderCell>{Strings.tourId}</Table.HeaderCell>
