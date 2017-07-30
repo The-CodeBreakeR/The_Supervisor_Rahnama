b21236600448b994f31a6141c40f44cf150806e4
Seyed Ali Tabatabaee <Ali>
2017-07-30 11:40:41 -0700

    debugging

--- accomm/models.py
+++ accomm/models.py
@@ -14,13 +14,13 @@ class AccommPrinciple(models.Model):
         return str(self.description)
 
 class Accommodation(models.Model):
-    size = models.IntegerField(default=50)
+    size = models.IntegerField(default=60)
     type = models.TextField(max_length=30)
     location = models.TextField(max_length=2000)
     contracted_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
                                       blank=True, related_name='contracted')
-    start_date = models.DateField(default=datetime.now, null=True, blank=True)
-    end_date = models.DateField(default=datetime.now, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
     reserved_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
                                     blank=True, related_name='reserved')
 
