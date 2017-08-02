32ebda96c2569640bf0c53339e7d93ff9899750f
smhssalehi <smhssalehi@gmail.com>
2017-08-02 06:42:31 -0700

    new list added

--- client/src/components/scheduling/SchedulingToday.js
+++ client/src/components/scheduling/SchedulingToday.js
@@ -1,73 +1,54 @@
 import React from 'react'
+import fetch from 'isomorphic-fetch'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 import Strings from '../../localization'
-import MomentJ from 'moment-jalaali'
-import Cookie from 'browser-cookies'
+
+import SchedulingSearch from './SchedulingSearch'
+import SchedulingList from './SchedulingList'
 
 class SchedulingToday extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      SchedulingId: '',
-      SchedulingName: '',
-      start: '',
-      end: '',
-      schedulingpec: '',
-      schedulingprice: '',
-      schedulingcapacity: '',
-      status: 4,
+      schedulingList: [],
+      error: '',
     }
   }
-  statusChecker(id) {
-    if (Cookie.get('token')) {
-      fetch('/scheduling/today/', {
+
+  handleResult(result) {
+    if (result.status === -1) {
+      this.setState({error: Strings.noSchedulingFound})
+      this.props.setSchedulingList([])
+    } else {
+      this.setState({error: ''})
+      this.props.setSchedulingList(result.scheduling)
+    }
+  }
+
+  search() {
+    if (true) {
+      fetch('/scheduling/week/', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          token: JSON.parse(localStorage.getItem('user')).token,
-          schedulingId: id,
         }),
       })
         .then(response => response.json())
-        .then(result => this.setState({status: result.status}))
-    }
-  }
-  getInfo() {
-    fetch('/scheduling/today/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        id: this.props.schedulingId,
-      }),
-    })
-      .then(response => response.json())
-      .then(result => {
+        .then(result => this.handleResult(result))
+        .then(result => {
         if (result.status === 0) {
-          console.log(result.scheduling)
-          this.setState({SchedulingID: result.scheduling.id})
-          this.setState({SchedulingName: result.scheduling.name})
-          this.setState({start: MomentJ(result.scheduling.start_time * 1000).format('LLLL')})
-          this.setState({end: MomentJ(result.scheduling.end_time * 1000).format('LLLL')})
-          this.setState({schedulingprice: result.scheduling.price})
-          this.setState({schedulingpec: result.scheduling.spec})
-          this.setState({schedulingcapacity: result.scheduling.capacity})
-          this.statusChecker(result.scheduling.id)
+          this.setState({schedulingList: result.scheduling})
         }
-      })
+    })
   }
-  setStatus(stat) {
-    this.setState({status: stat})
   }
 
   render() {
-    return <Modal trigger={<Button onClick={() => this.getInfo()}>{Strings.moreInfo}</Button>}>
-      <Modal.Header>{Strings.schedulingInfo}</Modal.Header>
+    return <Modal trigger={<Button onClick={() => this.search()}>{Strings.todayWorks}</Button>}>
+      <Modal.Header>{Strings.todayWorks}</Modal.Header>
       <Modal.Content image scrolling>
         <Image
           size='medium'
@@ -75,13 +56,8 @@ class SchedulingToday extends React.Component {
           wrapped
         />
         <Modal.Description>
-          <Header>{Strings.schedulingInfo}</Header>
-          <p>{Strings.schedulingName} : {this.state.SchedulingName}</p>
-          <p>{Strings.schedulingStartDate} : {this.state.start}</p>
-          <p>{Strings.schedulingEndDate} : {this.state.end}</p>
-          <p>{Strings.info} : {this.state.schedulingpec}</p>
-          <p>{Strings.schedulingPrice} : {this.state.schedulingprice}</p>
-          <p>{Strings.schedulingCapacity} : {this.state.schedulingcapacity}</p>
+          <Header>{Strings.todayWorks}</Header>
+          <SchedulingList schedulingList={this.state.schedulingList} />
           <p>{this.state.status}</p>
         </Modal.Description>
       </Modal.Content>
