41ae5ba3879811393d840d76f765f1b9ab01e2e1
Pooya Mirhosseini <pooyamirhosseini@gmail.com>
2017-08-02 16:14:47 +0430

    add commentshower

--- client/src/components/tours/CommentShower.js
+++ client/src/components/tours/CommentShower.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Button, Form, Icon, Image, Modal, Input, Comment } from 'semantic-ui-react'
+import { Button, Form, Icon, Image, Modal, Input, Comment,Segment } from 'semantic-ui-react'
 import _ from 'lodash'
 import Strings from '../../localization'
 import Cookie from 'browser-cookies'
@@ -16,13 +16,15 @@ class CommentShower extends React.Component {
   }
 
   renderComment(comment) {
-    return <Comment>
-      {/*<Comment.Avatar as='a' src='/assets/images/avatar/small/christian.jpg' />*/}
-      <Comment.Content>
-        <Comment.Author>{comment.name}</Comment.Author>
-        <Comment.Text>{comment.text}</Comment.Text>
-      </Comment.Content>
-    </Comment>
+    return <Segment>
+      <Comment>
+        {/*<Comment.Avatar as='a' src='/assets/images/avatar/small/christian.jpg' />*/}
+        <Comment.Content>
+          <Comment.Author>{comment.name}</Comment.Author>
+          <Comment.Text>{comment.text}</Comment.Text>
+        </Comment.Content>
+      </Comment>
+    </Segment>
   }
   onuserCommentChange(value) {
     this.setState({userComment: value})
@@ -38,6 +40,7 @@ class CommentShower extends React.Component {
         body: JSON.stringify({
           token: JSON.parse(localStorage.getItem('user')).token,
           comment: this.state.userComment,
+          tourId: this.props.tourId,
         }),
       })
         .then(response => response.json())
@@ -46,17 +49,19 @@ class CommentShower extends React.Component {
   }
   handleResult(result){
     this.props.getInfoRecall()
+    this.setState({userComment: ''})
   }
   render() {
     const comments = this.props.commnetList.map((comment) => this.renderComment(comment))
     return <div>
       <Comment.Group>
-        {Strings.toursComment}
-        {comments}
-        <Form >
-          {this.props.SubmitStatus !== 4 && <Form.TextArea value={this.state.userComment} placeholder={Strings.writeYourCommentTour} onChange={event => this.onuserCommentChange(event.target.value)}/>}
-          {this.props.SubmitStatus !== 4 && <Button content={Strings.submit} onClick={() => this.sendComment()} primary />}
-        </Form>
+        <Segment>{Strings.toursComment}
+          {comments}
+          <Form >
+            {this.props.SubmitStatus !== 4 && <Form.TextArea value={this.state.userComment} placeholder={Strings.writeYourCommentTour} onChange={event => this.onuserCommentChange(event.target.value)}/>}
+            {this.props.SubmitStatus !== 4 && <Button content={Strings.submit} onClick={() => this.sendComment()} primary />}
+          </Form>
+        </Segment>
       </Comment.Group>
     </div>
 
