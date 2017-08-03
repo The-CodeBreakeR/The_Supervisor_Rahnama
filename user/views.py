58c4047f29cc62400b6a0e0104699a19ed9013ac
Shervin Khastoo <shervin@shervin.org>
2017-08-03 18:47:18 +0430

    Add viewsets for profile models

--- user/views.py
+++ user/views.py
@@ -2,8 +2,9 @@ from rest_framework import viewsets
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .models import User
-from .serializers import UserSerializer
+from .models import User, PersonalProfile, EducationalProfile, SemesterInformation, CourseInformation
+from .serializers import UserSerializer, PersonalProfileSerializer, EducationalProfileSerializer,\
+    SemesterInformationSerializer, CourseInformationSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -20,3 +21,23 @@ class CustomObtainAuthToken(ObtainAuthToken):
         token = Token.objects.get(key=response.data['token'])
         name = token.user.first_name + ' ' + token.user.last_name
         return Response({'token': token.key, 'id': token.user_id, 'name': name})
+
+
+class PersonalProfileViewSet(viewsets.ModelViewSet):
+    queryset = PersonalProfile.objects.all()
+    serializer_class = PersonalProfileSerializer
+
+
+class EducationalProfileViewSet(viewsets.ModelViewSet):
+    queryset = EducationalProfile.objects.all()
+    serializer_class = EducationalProfileSerializer
+
+
+class SemesterInformationViewSet(viewsets.ModelViewSet):
+    queryset = SemesterInformation.objects.all()
+    serializer_class = SemesterInformationSerializer
+
+
+class CourseInformationViewSet(viewsets.ModelViewSet):
+    queryset = CourseInformation.objects.all()
+    serializer_class = CourseInformationSerializer
