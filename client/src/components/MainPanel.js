29ae51af388f6a6b5071871869f6e24d08b25ec2
Seyed Ali Tabatabaee <Ali>
2017-08-04 09:01:19 -0700

    mainpanel styling

--- client/src/components/MainPanel.js
+++ client/src/components/MainPanel.js
@@ -5,6 +5,7 @@
 import React from 'react'
 import Strings from '../localization'
 import { Link } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 import fetch from 'isomorphic-fetch'
 import Cookie from 'browser-cookies'
 
@@ -22,14 +23,14 @@ class MainPanel extends React.Component {
 
   render() {
     return <div className='mainpanel'>
-      <Link to='/tours'><Button primary>{Strings.toursub}</Button></Link>
-      <Link to='/skill'><Button color='green'>{Strings.skillsub}</Button></Link>
-      <Link to='/accounting'><Button primary>{Strings.accountingsub}</Button></Link>
-      <Link to='/accommodation'><Button color='green'>{Strings.accommodationsub}</Button></Link>
-      <Link to='/profile'><Button primary>{Strings.profilesub}</Button></Link>
-      <Link to='/internship'><Button color='green'>{Strings.internshipsub}</Button></Link>
-      <Link to='/timing'><Button primary>{Strings.timingsub}</Button></Link>
-      <Link to='/scheduling'><Button color='green'>{Strings.schedulingsub}</Button></Link>
+      <Link to='/tours'><Button className='mainpanel__button' primary>{Strings.toursub}</Button></Link>
+      <Link to='/skill'><Button className='mainpanel__button' color='green'>{Strings.skillsub}</Button></Link>
+      <Link to='/accounting'><Button className='mainpanel__button' primary>{Strings.accountingsub}</Button></Link>
+      <Link to='/accommodation'><Button className='mainpanel__button' color='green'>{Strings.accommodationsub}</Button></Link>
+      <Link to='/profile'><Button className='mainpanel__button' primary>{Strings.profilesub}</Button></Link>
+      <Link to='/internship'><Button className='mainpanel__button' color='green'>{Strings.internshipsub}</Button></Link>
+      <Link to='/timing'><Button className='mainpanel__button' primary>{Strings.timingsub}</Button></Link>
+      <Link to='/scheduling'><Button className='mainpanel__button' color='green'>{Strings.schedulingsub}</Button></Link>
     </div>
   }
 }
