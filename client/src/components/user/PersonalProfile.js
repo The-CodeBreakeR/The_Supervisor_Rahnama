af6e90c18d8e944b232f1b53748aba7197ad02c5
Shervin Khastoo <shervin@shervin.org>
2017-08-04 18:45:56 +0430

    Complete personal profile form

--- client/src/components/user/PersonalProfile.js
+++ client/src/components/user/PersonalProfile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import fetch from 'isomorphic-fetch'
 import Strings from '../../localization'
-import {formatError} from './utils'
+import {formatError, getUser} from './utils'
 import {Button, Modal, Form, Message} from 'semantic-ui-react'
 
 class RegistrationModal extends React.Component {
@@ -19,9 +19,10 @@ class RegistrationModal extends React.Component {
       birthPlace: {value: '', error: false},
       nationalId: {value: '', error: false},
       mobileNumber: {value: '', error: false},
-      maritalStatus: {value: false, error: false},
+      maritalStatus: {value: '', error: false},
       landlineNumber: {value: '', error: false},
       address: {value: '', error: false},
+      error: '',
     }
   }
 
@@ -33,6 +34,29 @@ class RegistrationModal extends React.Component {
     this.setState({birthDate: {value, error: !/\d{4}\/\d{1,2}\/\d{1,2}/.test(value)}})
   }
 
+  onBirthPlaceChanged(value) {
+    this.setState({birthPlace: {value, error: value.length < 3}})
+  }
+
+  onNationalIdChanged(value) {
+    this.setState({nationalId: {value, error: !/\d{10}/.test(value)}})
+  }
+
+  onMobileNumberChanged(value) {
+    this.setState({mobileNumber: {value, error: !/^(\+|0|98|\+98|0098)?9\d{9}$/.test(value)}})
+  }
+
+  onLandlineNumberChanged(value) {
+    this.setState({landlineNumber: {value, error: !/^(\+|0|98|\+98|0098)?\d{10}$/.test(value)}})
+  }
+
+  onMaritalStatusChanged(value) {
+    this.setState({maritalStatus: {value, error: value !== 'S' && value !== 'M'}})
+  }
+
+  onAddressChanged(value) {
+    this.setState({address: {value, error: value.length < 10}})
+  }
 
   isOK() {
     for (const field of this.fields) {
@@ -44,51 +68,63 @@ class RegistrationModal extends React.Component {
   }
 
   validate() {
-    this.onStudentIdChanged(this.state.studentId.value)
-    this.onEmailChanged(this.state.email.value)
-    this.onPasswordChanged(this.state.password.value)
-    this.onConfirmPasswordChanged(this.state.confirmPassword.value)
-    this.onFirstNameChanged(this.state.firstName.value)
-    this.onLastNameChanged(this.state.lastName.value)
+    this.onFatherNameChanged(this.state.fatherName.value)
+    this.onBirthDateChanged(this.state.birthDate.value)
+    this.onBirthPlaceChanged(this.state.birthPlace.value)
+    this.onNationalIdChanged(this.state.nationalId.value)
+    this.onMobileNumberChanged(this.state.mobileNumber.value)
+    this.onLandlineNumberChanged(this.state.landlineNumber.value)
+    this.onMaritalStatusChanged(this.state.maritalStatus.value)
+    this.onAddressChanged(this.state.address.value)
   }
 
   generateErrors() {
     let errors = this.state.error
-    if (this.state.studentId.error) {
-      errors += formatError(Strings.studentIdError)
+    if (this.state.fatherName.error) {
+      errors += formatError(Strings.fatherNameError)
     }
-    if (this.state.email.error) {
-      errors += formatError(Strings.emailError)
+    if (this.state.birthDate.error) {
+      errors += formatError(Strings.birthDateError)
     }
-    if (this.state.password.error) {
-      errors += formatError(Strings.passwordError)
+    if (this.state.birthPlace.error) {
+      errors += formatError(Strings.birthPlaceError)
     }
-    if (this.state.confirmPassword.error) {
-      errors += formatError(Strings.confirmPasswordError)
+    if (this.state.nationalId.error) {
+      errors += formatError(Strings.nationalIdError)
     }
-    if (this.state.firstName.error || this.state.lastName.error) {
-      errors += formatError(Strings.nameError)
+    if (this.state.mobileNumber.error) {
+      errors += formatError(Strings.mobileNumberError)
     }
-    if (this.state.rules.error) {
-      errors += formatError(Strings.rulesError)
+    if (this.state.landlineNumber.error) {
+      errors += formatError(Strings.landlineNumberError)
+    }
+    if (this.state.maritalStatus.error) {
+      errors += formatError(Strings.maritalStatusError)
+    }
+    if (this.state.address.error) {
+      errors += formatError(Strings.addressError)
     }
     return errors
   }
 
-  register() {
+  submit() {
     if (this.isOK()) {
-      fetch('/api/user/', {
+      fetch('/api/personal_profile/', {
         method: 'POST',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
-          username: this.state.studentId.value,
-          email: this.state.email.value,
-          password: this.state.password.value,
-          first_name: this.state.firstName.value,
-          last_name: this.state.lastName.value,
+          user: `/api/user/${getUser().id}/`,
+          fatherName: this.state.fatherName.value,
+          birthDate: this.state.birthDate.value,
+          birthPlace: this.state.birthPlace.value,
+          nationalId: this.state.nationalId.value,
+          mobileNumber: this.state.mobileNumber.value,
+          landlineNumber: this.state.landlineNumber.value,
+          maritalStatus: this.state.maritalStatus.value,
+          address: this.state.address.value,
         }),
       })
         .then(response => response.json())
@@ -97,7 +133,7 @@ class RegistrationModal extends React.Component {
   }
 
   handleResult(result) {
-    if (result.id) {
+    if (result.url) {
       this.setState({done: true})
     } else if (result.username) {
       this.setState({error: formatError(Strings.studentIdAlreadyExists)})
@@ -111,63 +147,78 @@ class RegistrationModal extends React.Component {
         <Form>
           <Form.Group widths='equal'>
             <Form.Input
-              label={Strings.studentId}
-              value={this.state.studentId.value}
-              error={this.state.studentId.error}
-              onChange={event => this.onStudentIdChanged(event.target.value)}
-              placeholder={Strings.studentId}
+              label={Strings.fatherName}
+              value={this.state.fatherName.value}
+              error={this.state.fatherName.error}
+              onChange={event => this.onFatherNameChanged(event.target.value)}
+              placeholder={Strings.fatherName}
             />
             <Form.Input
-              label={Strings.email}
-              value={this.state.email.value}
-              error={this.state.email.error}
-              onChange={event => this.onEmailChanged(event.target.value)}
-              placeholder={Strings.email}
+              label={Strings.nationalId}
+              value={this.state.nationalId.value}
+              error={this.state.nationalId.error}
+              onChange={event => this.onNationalIdChanged(event.target.value)}
+              placeholder={Strings.nationalId}
             />
           </Form.Group>
           <Form.Group widths='equal'>
             <Form.Input
-              label={Strings.firstName}
-              value={this.state.firstName.value}
-              error={this.state.firstName.error}
-              onChange={event => this.onFirstNameChanged(event.target.value)}
-              placeholder={Strings.firstName}
+              label={Strings.birthDate}
+              value={this.state.birthDate.value}
+              error={this.state.birthDate.error}
+              onChange={event => this.onBirthDateChanged(event.target.value)}
+              placeholder={Strings.birthDate}
             />
             <Form.Input
-              label={Strings.lastName}
-              value={this.state.lastName.value}
-              error={this.state.lastName.error}
-              onChange={event => this.onLastNameChanged(event.target.value)}
-              placeholder={Strings.lastName}
+              label={Strings.birthPlace}
+              value={this.state.birthPlace.value}
+              error={this.state.birthPlace.error}
+              onChange={event => this.onBirthPlaceChanged(event.target.value)}
+              placeholder={Strings.birthPlace}
             />
           </Form.Group>
           <Form.Group widths='equal'>
             <Form.Input
-              label={Strings.password}
-              value={this.state.password.value}
-              error={this.state.password.error}
-              onChange={event => this.onPasswordChanged(event.target.value)}
-              placeholder={Strings.password}
+              label={Strings.mobileNumber}
+              value={this.state.mobileNumber.value}
+              error={this.state.mobileNumber.error}
+              onChange={event => this.onMobileNumberChanged(event.target.value)}
+              placeholder={Strings.mobileNumber}
               type='password'
             />
             <Form.Input
-              label={Strings.confirmPassword}
-              value={this.state.confirmPassword.value}
-              error={this.state.confirmPassword.error}
-              onChange={event => this.onConfirmPasswordChanged(event.target.value)}
-              placeholder={Strings.confirmPassword}
+              label={Strings.landlineNumber}
+              value={this.state.landlineNumber.value}
+              error={this.state.landlineNumber.error}
+              onChange={event => this.onLandlineNumberChanged(event.target.value)}
+              placeholder={Strings.landlineNumber}
               type='password'
             />
           </Form.Group>
-          <Form.Checkbox
-            label={Strings.iAgreeWithRules}
-            checked={this.state.rules.value}
-            error={this.state.rules.error}
-            onClick={() => this.onRulesChanged()}
+          <Form.Select
+            label={Strings.maritalStatus}
+            placeholder={Strings.maritalStatus}
+            value={this.state.maritalStatus.value}
+            error={this.state.maritalStatus.error}
+            options={[{key: 'S', value: 'S', text: Strings.single}, {key: 'M', value: 'M', text: Strings.married}]}
+            onChange={(_, obj) => this.onMaritalStatusChanged(obj.value)}
+          />
+          <Form.TextArea
+            label={Strings.address}
+            value={this.state.address.value}
+            error={this.state.address.error}
+            onChange={event => this.onAddressChanged(event.target.value)}
+            placeholder={Strings.address}
           />
         </Form>
-        <Button primary onClick={() => this.register()}>{Strings.registration}</Button>
-        <Button secondary onClick={() => this.close()}>{Strings.cancel}</Button>
+        {errors && <Message
+            error
+            content={errors}
+          />
+        }
+        <div className="personal-profile__submit">
+          <Button primary onClick={() => this.submit()}>{Strings.submit}</Button>
+        </div>
       </div>
     )
   }
