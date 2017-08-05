80a338275fda40787b3e93d2700dc61adad83e8a
Shervin Khastoo <shervin@shervin.org>
2017-08-05 19:46:29 +0430

    Remove buggy import

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
 import 'semantic-ui-less/semantic.less'
-import 'react-datepicker2/dist/react-datepicker2.min.css'
 
 import App from './components/App'
 
