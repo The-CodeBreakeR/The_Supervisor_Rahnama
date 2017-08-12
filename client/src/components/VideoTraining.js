60b05e385355a7301986842c7ca3fac91924a744
smhssalehi <smhssalehi@gmail.com>
2017-08-12 20:17:50 +0430

    video modal added

--- client/src/components/VideoTraining.js
+++ client/src/components/VideoTraining.js
@@ -13,18 +13,18 @@ class VideoTraining extends React.Component {
 
   render() {
     const logo = <div className='app__header__training' color="red">
-        <Button color="red">{Strings.training}</Button> 
+        <Button color="red">{Strings.training}</Button>
     </div>
 
-    // const logo_big = <video controls="controls">
-    //   <source src="https://drive.google.com/uc?export=download&id=0B0JMGMGgxp9WMEdWb1hyQUhlOWs" type='video/mp4'/>
-    // </video>
+    const logo_big = <video controls="controls">
+      <source src="https://drive.google.com/uc?export=download&id=0B0JMGMGgxp9WMEdWb1hyQUhlOWs" type='video/mp4'/>
+    </video>
     // const logo = <Image className='app__header__column__logo' src={rahLogo} alt={'no Image load'}/>
     return <Modal closeIcon trigger={logo}>
       <Modal.Header>{Strings.rah}</Modal.Header>
       <Modal.Content image scrolling>
         <Modal.Description className='app__logo__modal'>
-          {/*{logo_big}*/}
+          {logo_big}
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
