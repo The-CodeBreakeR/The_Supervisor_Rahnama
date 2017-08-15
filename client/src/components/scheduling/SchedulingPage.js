b9d4e790b054f241d8c0f558c8ec838782bb7c8c
smhssalehi <smhssalehi@gmail.com>
2017-08-16 00:24:07 +0430

    request sched change from modal to static

--- client/src/components/scheduling/SchedulingPage.js
+++ client/src/components/scheduling/SchedulingPage.js
@@ -9,26 +9,27 @@ import HardDayModal from './HardDayModal'
 // import { Calendar, DatePicker } from 'react-persian-datepicker'
 
 import moment from 'moment-jalaali'
-import DatePicker from 'react-datepicker2';
+import DatePicker from 'react-datepicker2'
 
-import 'react-datepicker2/dist/react-datepicker2.min.css';
+import 'react-datepicker2/dist/react-datepicker2.min.css'
 import MomentJ from 'moment-jalaali'
 class SchedulingPage extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       schedulingList: [],
       list: '',
-      value:  moment(),
+      value: moment(),
       name: '',
       url: '/scheduling/all/',
       checked: 'all',
+      page: 'main',
       // search: false,
     }
   }
 
-  componentWillMount() {
-    console.log("uu",this.state.name,this.state.url)
+  componentWillMount () {
+    console.log('uu', this.state.name, this.state.url)
     fetch(this.state.url, {
       method: 'POST',
       headers: {
@@ -42,8 +43,8 @@ class SchedulingPage extends React.Component {
       .then(response => response.json())
       .then(result => {
         // if(result.status === 0) {
-          console.log("cmw",result.scheduling)
-          this.setState({schedulingList: result.scheduling})
+        console.log('cmw', result.scheduling)
+        this.setState({schedulingList: result.scheduling})
         // }else{
         //   alert("-1 status")
         // }
@@ -54,52 +55,63 @@ class SchedulingPage extends React.Component {
   //   this.setState({schedulingList: schedulingList})
   // }
 
-  setUrl(event) {
+  setUrl (event) {
     // console.log("ddd",{checked: event.target.value,name: '',url: '/scheduling/'+event.target.value+'/'})
-    this.setState({url: '/scheduling/'+event.target.value+'/',checked: event.target.value,name: ''},function () {this.componentWillMount()})
+    this.setState({
+      url: '/scheduling/' + event.target.value + '/',
+      checked: event.target.value,
+      name: ''
+    }, function () {this.componentWillMount()})
     // alert(this.state.url)
   }
 
-  search(event) {
+  search (event) {
     // console.log("tt",event.target.value)
-    this.setState({name: event.target.value,url: '/scheduling/search/',checked:'search'},function () {this.componentWillMount()})
+    this.setState({
+      name: event.target.value,
+      url: '/scheduling/search/',
+      checked: 'search'
+    }, function () {this.componentWillMount()})
     // this.setState({url: '/scheduling/search/'})
     // this.componentWillMount()
   }
 
-  render() {
+  render () {
+    alert(this.state.page)
     return <div className='scheduling'>
- <DatePicker
-      onChange={value => this.setState({ value })}
-      value={this.state.value}
-    />
-      <Grid centered>
+      {this.state.page === 'main' && <Grid centered>
         <Grid.Row columns={2}>
-
-           <HardDayModal onLogin={() => this.forceUpdate()}/>
+          <HardDayModal onLogin={() => this.forceUpdate()}/>
         </Grid.Row>
         <Grid.Row columns={4}>
 
           <div onChange={this.setUrl.bind(this)}>
-            <Input type='radio' value='all' name='list' checked={this.state.checked === 'all'}/> {Strings.schedulingAll} &emsp;
-            <Input type='radio' value='today' name='list' checked={this.state.checked === 'today'}/> {Strings.todayWorks} &emsp;
-            <Input type='radio' value='week' name='list' checked={this.state.checked === 'week'}/>{Strings.schedulingWeekDisplay} &emsp;
-            <Input type='radio' value='month' name='list' checked={this.state.checked === 'month'}/> {Strings.schedulingMonthDisplay} &emsp;
-            <Input type='radio' value='search' name='list' checked={this.state.checked === 'search'} /> {Strings.search} &emsp;
+            <Input type='radio' value='all' name='list'
+                   checked={this.state.checked === 'all'}/> {Strings.schedulingAll} &emsp;
+            <Input type='radio' value='today' name='list'
+                   checked={this.state.checked === 'today'}/> {Strings.todayWorks} &emsp;
+            <Input type='radio' value='week' name='list'
+                   checked={this.state.checked === 'week'}/>{Strings.schedulingWeekDisplay} &emsp;
+            <Input type='radio' value='month' name='list'
+                   checked={this.state.checked === 'month'}/> {Strings.schedulingMonthDisplay} &emsp;
+            <Input type='radio' value='search' name='list'
+                   checked={this.state.checked === 'search'}/> {Strings.search} &emsp;
           </div>
           <div>
             {this.state.checked === 'search' && <Input value={this.state.name} placeholder={Strings.schedulingName}
-                   onChange={event => this.search(event)}/>}
+                                                       onChange={event => this.search(event)}/>}
           </div>
         </Grid.Row>
         <Grid.Row centered>
           <SchedulingList schedulingList={this.state.schedulingList}/>
         </Grid.Row>
         <Grid.Row columns={2}>
-          <SchedulingRequest/>
+          <button onClick={this.setState({page: 'request'})}>سلام{Strings.schedulingNewItem}</button>
         </Grid.Row>
       </Grid>
-    </div>
+      }{this.state.page === 'request' &&
+    <SchedulingRequest page={this.state.page}/>
+    }</div>
   }
 }
 
--- client/src/components/scheduling/SchedulingRequest.js
+++ client/src/components/scheduling/SchedulingRequest.js
@@ -5,12 +5,10 @@ import Strings from '../../localization'
 import { Button, Header, Image, Modal } from 'semantic-ui-react'
 // import { Calendar, DatePicker } from 'react-persian-datepicker';
 import css from '../../../stylesheet/basics.css'
-import DatePicker from 'react-datepicker2';
-
-
+import DatePicker from 'react-datepicker2'
 
 class SchedulingRequest extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
     this.state = {
       name: '',
@@ -22,20 +20,25 @@ class SchedulingRequest extends React.Component {
       accept: '',
     }
   }
-  onInfoChange(value) {
+
+  onInfoChange (value) {
     this.setState({info: value})
   }
-  onNameChange(value) {
+
+  onNameChange (value) {
     this.setState({name: value})
   }
-  onCapasityChange(value) {
+
+  onCapasityChange (value) {
     this.setState({capasity: value})
   }
-  onTimeChange(value) {
+
+  onTimeChange (value) {
     console.log('here1')
     this.setState({end_date: value})
   }
-  handleResult(result) {
+
+  handleResult (result) {
     if (result.status === 0) {
       this.setState({error: ''})
       this.setState({accept: Strings.requestSubmitAccept})
@@ -45,12 +48,13 @@ class SchedulingRequest extends React.Component {
     }
     this.setState({request: ''})
   }
-  sendRequest() {
+
+  sendRequest () {
     console.log('here')
     if (true) {
       this.setState({open: false})
       console.log(this.state.name)
-      console.log(MomentJ(this.state.end_date, 'jYYYY/jMM/jDD').toDate().getTime()/1000)
+      console.log(MomentJ(this.state.end_date, 'jYYYY/jMM/jDD').toDate().getTime() / 1000)
       console.log(this.state.info)
       console.log(this.state.capasity)
       fetch('/scheduling/request/', {
@@ -61,9 +65,9 @@ class SchedulingRequest extends React.Component {
         },
         body: JSON.stringify({
           name: this.state.name,
-          end_date:MomentJ(this.state.end_date, 'jYYYY/jMM/jDD').toDate().getTime()/1000,
-          capasity:this.state.capasity,
-          info:this.state.info,
+          end_date: MomentJ(this.state.end_date, 'jYYYY/jMM/jDD').toDate().getTime() / 1000,
+          capasity: this.state.capasity,
+          info: this.state.info,
         }),
       })
         .then(response => response.json())
@@ -71,36 +75,32 @@ class SchedulingRequest extends React.Component {
     }
   }
 
-  render() {
-    console.log("gd", css)
-    return <Modal
-      closeIcon
-      open={this.state.open}
-      onOpen={() => this.setState({open: true})}
-      onClose={() => this.setState({open: false})}
-      trigger={<Button>{Strings.schedulingNewItem}</Button>}
-    >
-      <Modal.Header>{Strings.schedulingNewItem}</Modal.Header>
-      <Modal.Content >
-        <Modal.Description>
-          <div>
-            <div>
-              <Form>
-                <Form.Input value={this.state.name} label={Strings.schedulingName} placeholder={Strings.requestForScheduling} onChange={event => this.onNameChange(event.target.value)} />
-                <Form.Input value={this.state.capasity} label={Strings.schedulingCapasity} placeholder={Strings.requestForScheduling} onChange={event => this.onCapasityChange(event.target.value)} />
-                <Form.Input value={this.state.capasity} label={Strings.schedulingCapasity} placeholder={Strings.requestForScheduling} onChange={event => this.onCapasityChange(event.target.value)} />
-                <DatePicker />
-                <Form.Input value={this.state.end_date} label={Strings.schedulingEndDate} placeholder={Strings.timeFormat} onChange={event => this.onTimeChange(event.target.value)} />
-                <Form.TextArea value={this.state.info} label={Strings.info} placeholder={Strings.requestForScheduling} onChange={event => this.onInfoChange(event.target.value)} />
-                <Form.Button onClick={() => this.sendRequest()} >{Strings.submit}</Form.Button>
-              </Form>
-            </div>
-          </div>
-        </Modal.Description>
-      </Modal.Content>
-      <Modal.Actions>
-      </Modal.Actions>
-    </Modal>
+  render () {
+    console.log('gd', css)
+    return <div>
+      <Header>{Strings.schedulingNewItem}</Header>
+      <div>
+        <div>
+          <Form>
+            <Form.Input value={this.state.name} label={Strings.schedulingName}
+                        placeholder={Strings.requestForScheduling}
+                        onChange={event => this.onNameChange(event.target.value)}/>
+            <Form.Input value={this.state.capasity} label={Strings.schedulingCapasity}
+                        placeholder={Strings.requestForScheduling}
+                        onChange={event => this.onCapasityChange(event.target.value)}/>
+            <Form.Input value={this.state.capasity} label={Strings.schedulingCapasity}
+                        placeholder={Strings.requestForScheduling}
+                        onChange={event => this.onCapasityChange(event.target.value)}/>
+            <DatePicker />
+            <Form.Input value={this.state.end_date} label={Strings.schedulingEndDate} placeholder={Strings.timeFormat}
+                        onChange={event => this.onTimeChange(event.target.value)}/>
+            <Form.TextArea value={this.state.info} label={Strings.info} placeholder={Strings.requestForScheduling}
+                           onChange={event => this.onInfoChange(event.target.value)}/>
+            <Form.Button onClick={() => this.sendRequest()}>{Strings.submit}</Form.Button>
+          </Form>
+        </div>
+      </div>
+    </div>
   }
 }
 
