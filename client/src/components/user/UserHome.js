a49f2951056dc5573961e764a697c8a41bff81aa
Shervin Khastoo <shervinkh145@gmail.com>
2017-08-25 21:18:50 +0430

    Enable subsystems on complete profile

--- client/src/components/user/UserHome.js
+++ client/src/components/user/UserHome.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, withRouter } from 'react-router-dom'
 
 import ProfileButtons from './ProfileButtons'
 import PersonalProfile from './PersonalProfile'
@@ -7,12 +7,15 @@ import EducationalProfile from './EducationalProfile'
 
 class UserHome extends React.Component {
   render() {
+    const { history, updateProfile } = this.props
     return <Switch>
       <Route exact path='/profile' component={ProfileButtons}/>
-      <Route path='/profile/personal' component={PersonalProfile}/>
-      <Route path='/profile/educational' component={EducationalProfile}/>
+      <Route path='/profile/personal' render={() => <PersonalProfile updateProfile={updateProfile} history={history} />}/>
+      <Route path='/profile/educational' render={() => <EducationalProfile updateProfile={updateProfile} history={history}/>}/>
     </Switch>
   }
 }
 
-export default UserHome
+const withRouterUserHome = withRouter(UserHome)
+
+export default withRouterUserHome
