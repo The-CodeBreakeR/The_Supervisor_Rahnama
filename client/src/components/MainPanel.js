716d94a79e134a786e4924bbef728deebdaf98de
Shervin Khastoo <shervinkh145@gmail.com>
2017-08-25 22:48:20 +0430

    Show user info on main panel

--- client/src/components/MainPanel.js
+++ client/src/components/MainPanel.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Strings from '../localization'
+import MomentJ from 'moment-jalaali'
 import { Link } from 'react-router-dom'
-
+import { getDegree, getMajor } from './user/utils'
 import { Button, Grid, Icon } from 'semantic-ui-react'
 import Subsystem from './Subsystem'
 
@@ -11,12 +12,18 @@ class MainPanel extends React.Component {
     const userGender = user && user.gender
     const userPrefix = userGender === 'F' ? Strings.mrs : Strings.mr
     const userName = user && `${user.first_name} ${user.last_name}`
+    const thisYear = MomentJ().jYear()
 
     return (user.personal_profile && user.educational_profile)
       ? <div>
           <div>
             <Icon name='user' size='massive' />
-            <div>{`${userPrefix} ${userName}`}</div>
+            <div className='main-panel__user-data'>
+              <div>{`${userPrefix} ${userName}`}</div>
+              <div>{`${getDegree(user.educational_profile.degree)} ${getMajor(user.educational_profile.major)}`}</div>
+              <div>{`${Strings.born} ${user.personal_profile.birth_date.split('/')[0]}`}</div>
+              <div>{`${Strings.year} ${thisYear - user.educational_profile.entrance_year + 1}`}</div>
+            </div>
           </div>
           <Grid className='mainpanel'>
             <Grid.Row>
--- client/src/components/user/utils.js
+++ client/src/components/user/utils.js
@@ -1,4 +1,6 @@
 import Cookie from 'browser-cookies'
+import Strings from '../../localization'
+
 
 export function formatError(error) {
   return `\u2219 ${error} \n`
@@ -6,4 +8,31 @@ export function formatError(error) {
 
 export function getUser() {
   return Cookie.get('token') && JSON.parse(localStorage.getItem('user'))
+}
+
+export function getDegree(degree) {
+  const map = {
+    BSC: Strings.bachelor,
+    MSC: Strings.master,
+    PHD: Strings.doctoral,
+  }
+
+  return map[degree]
+}
+
+export function getMajor(major) {
+  const map = {
+    CE: Strings.computerEngineering,
+    CS: Strings.computerScience,
+    IE: Strings.industrialEngineering,
+    ME: Strings.mechanicalEngineering,
+    MA: Strings.mathematics,
+    PH: Strings.physics,
+    CH: Strings.chemistry,
+    CI: Strings.civil,
+    MN: Strings.management,
+    EC: Strings.economics,
+  }
+
+  return map[major]
 }
