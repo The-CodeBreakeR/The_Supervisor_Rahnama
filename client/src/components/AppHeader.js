6f8751dbf3df1d52ab4425d87a2f09c7a55dcb98
smhssalehi <smhssalehi@gmail.com>
2017-08-26 16:35:09 +0430

    header background added

--- client/src/components/AppHeader.js
+++ client/src/components/AppHeader.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import Strings from '../localization'
-import { Header, Segment } from 'semantic-ui-react'
+import { Header, Segment, Grid } from 'semantic-ui-react'
 import LogoInfo from './LogoInfo.js'
 import VideoTraining from './VideoTraining.js'
 import Timer from './Timer.js'
-
 class AppHeader extends React.Component {
-  componentWillMount() {
+  componentWillMount () {
     this.timerRef = setInterval(() => this.forceUpdate(), 1000)
   }
 
-
-  render() {
-    return <Segment className='app__header'>
-      <LogoInfo/>
-      <div style={ { marginLeft: '8px' } }>
-        <Header className='app__header__title'>{Strings.rahnamaSystem}</Header>
-      </div>
-      <Timer/>
-      {location.pathname === '/' && <VideoTraining/>}
-    </Segment>
+  render () {
+    return <div className='app__header'>
+      <Grid>
+        <Grid.Row className="Row__header">
+          <div className="right__header">
+            <LogoInfo/>
+            <div style={ {marginLeft: '8px'} }>
+              <Header className='app__header__title'>{Strings.rahnamaSystem}</Header>
+            </div>
+          </div>
+          <div className="left__header">
+            {location.pathname === '/' && <VideoTraining/>}
+            <Timer/>
+          </div>
+        </Grid.Row>
+      </Grid>
+    </div>
   }
 }
 
